96ef2f3d50336b3e37d622f4df7310c1b4ec5a98
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-11-05 23:23:07 +0100

    separated the todo controllers

---
+++ controllers/todos.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"ajafik.com/todo/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func GetTodos(c *gin.Context, db *gorm.DB) {
+	var todos []models.Todo
+	db.Find(&todos)
+	c.JSON(200, todos)
+}
+
+func CreateTodo(c *gin.Context, db *gorm.DB) {
+	var todo models.Todo
+	if err := c.BindJSON(&todo); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	db.Create(&todo)
+	c.JSON(201, todo)
+}
+
+func GetTodo(c *gin.Context, db *gorm.DB) {
+	var todo models.Todo
+	todoId := c.Param("id")
+	if err := db.Where("id = ?", todoId).First(&todo).Error; err != nil {
+		c.JSON(404, gin.H{"error": "Todo not found"})
+		return
+	}
+	c.JSON(200, todo)
+}
+
+func UpdateTodo(c *gin.Context, db *gorm.DB) {
+	var todo models.Todo
+	todoId := c.Param("id")
+	if err := db.Where("id = ?", todoId).First(&todo).Error; err != nil {
+		c.JSON(404, gin.H{"error": "Todo not found"})
+		return
+	}
+	if err := c.BindJSON(&todo); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	db.Save(&todo)
+	c.JSON(200, todo)
+}
+
+func DeleteTodo(c *gin.Context, db *gorm.DB) {
+	var todo models.Todo
+	todoId := c.Param("id")
+	if err := db.Where("id = ?", todoId).First(&todo).Error; err != nil {
+		c.JSON(404, gin.H{"error": "Todo not found"})
+		return
+	}
+	db.Delete(&todo)
+	c.JSON(200, gin.H{"message": "Todo deleted"})
+}
--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"strconv"
 
+	"ajafik.com/todo/controllers"
 	"ajafik.com/todo/middlewares"
 	"ajafik.com/todo/models"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
-
 
 func main() {
 	router := gin.Default()
@@ -25,55 +25,23 @@ func main() {
 	db.AutoMigrate(&models.Todo{})
 
 	router.GET("/todos", func(c *gin.Context) {
-		var todos []models.Todo
-		db.Find(&todos)
-		c.JSON(200, todos)
+		controllers.GetTodos(c, db)
 	})
 
 	router.POST("/todos", func(c *gin.Context) {
-		var todo models.Todo
-		if err := c.BindJSON(&todo); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-		db.Create(&todo)
-		c.JSON(201, todo)
+		controllers.CreateTodo(c, db)
 	})
 
 	router.GET("/todos/:id", func(c *gin.Context) {
-		var todo models.Todo
-		todoId := c.Param("id")
-		if err := db.Where("id = ?", todoId).First(&todo).Error; err != nil {
-			c.JSON(404, gin.H{"error": "Todo not found"})
-			return
-		}
-		c.JSON(200, todo)
+		controllers.GetTodo(c, db)
 	})
 
 	router.PUT("/todos/:id", func(c *gin.Context) {
-		var todo models.Todo
-		todoId := c.Param("id")
-		if err := db.Where("id = ?", todoId).First(&todo).Error; err != nil {
-			c.JSON(404, gin.H{"error": "Todo not found"})
-			return
-		}
-		if err := c.BindJSON(&todo); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-		db.Save(&todo)
-		c.JSON(200, todo)
+		controllers.UpdateTodo(c, db)
 	})
 
 	router.DELETE("/todos/:id", func(c *gin.Context) {
-		var todo models.Todo
-		todoId := c.Param("id")
-		if err := db.Where("id = ?", todoId).First(&todo).Error; err != nil {
-			c.JSON(404, gin.H{"error": "Todo not found"})
-			return
-		}
-		db.Delete(&todo)
-		c.JSON(200, gin.H{"message": "Todo deleted"})
+		controllers.DeleteTodo(c, db)
 	})
 
 	router.POST("/json", func(c *gin.Context) {
